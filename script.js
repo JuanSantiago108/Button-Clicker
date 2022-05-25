b57f4bf082a4c092d98af175b31ee74607ea4f3b
JuanSantiago108 <santiagojuanandres@gmail.com>
2022-05-24 22:54:26 -0400

    Button Clicker

---
+++ script.js
@@ -0,0 +1,21 @@
+function custom() {
+    // we can include more code here if we want to
+    console.log("this message is coming from script.js");
+}
+
+
+function logout(element) {
+    element.innerText = "Logout" ;
+}
+
+
+function define(element) {
+    element.remove();
+}
+
+
+// function changetext (element) {
+//     console.log ("Ninja was liked")
+// }
+
+
